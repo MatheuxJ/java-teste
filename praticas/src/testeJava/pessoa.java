98cb8101267658b3fb65a7996111a7e037bb7016
Matheus Juliano <91209849+MatheuxJ@users.noreply.github.com>
2023-06-27 22:00:38 -0400

    add alteracao

--- praticas/src/testeJava/pessoa.java
+++ praticas/src/testeJava/pessoa.java
@@ -2,9 +2,35 @@ package testeJava;
 
 public class pessoa {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		public String getNome() {
+		return nome;
 	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+		private String nome;
+		
+		public String getIdade() {
+			return idade;
+		}
+
+		public void setIdade(String idade) {
+			this.idade = idade;
+		}
+
+		private String sobrenome;
+		
+		private String idade;
+		
 
 }
